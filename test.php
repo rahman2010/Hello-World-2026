febfb7ca129d561606fe0a3fe7237729a8ecaa1c
rahman2010 <afzal2277rb@yahoo.com>
2026-05-18 16:05:36 +0900

    zzzzzzzzz

---
+++ test.php
@@ -0,0 +1,7 @@
+<? php
+echo "aaaaa";
+echo "aaaaa";
+git flow release start aaaa1 main
+Fatal: There is an existing release branch 'helloword1'. Finish that one first.
+Completed with errors, see above.
+

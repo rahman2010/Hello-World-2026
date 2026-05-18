821d4883f2d50bf1d7dbb7b34314a9a07060e282
rahman2010 <afzal2277rb@yahoo.com>
2026-05-18 16:40:32 +0900

    error1

--- test.php
+++ test.php
@@ -4,10 +4,6 @@ echo "aaaaa";
 
 echo "aaaaa";
 
-git flow release start aaaa1 main
-Fatal: There is an existing release branch 'helloword1'. Finish that one first.
-Completed with errors, see above.
-Completed with errors, see above.
-Completed with errors, see above.
+i an checking error.
 
 

ab79b6db8862672d9183678e482f88bb35f6f819
rahman2010 <afzal2277rb@yahoo.com>
2026-05-18 15:46:43 +0900

    dddddddddd

--- aa.php
+++ aa.php
@@ -1,27 +1,2 @@
 <? php
-
-echo "aaaaa bbbbb dev ";
-echo "it is after changing error";
-echo "it is after changing error11";
-
-echo "it is after changing error11";
-echo "it is after changing error22";
-
-echo "it is after changing error11";
-echo "it is after changing error33";
-
-
-echo "Test 2022";
-echo "it is after changing error33";
-
-echo "Test 2022";
-echo "it is after changing error33";
-
-echo "Test 2022 heloworld1 111111";
-echo "it is after changing error33";
-
-
-echo "Test 2022 heloworld1 111111";
-echo "it is after changing error33";
-
-
+echo "aaaaa";
